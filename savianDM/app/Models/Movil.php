d2e6988a8569492270866a9465691a6e08fd28d6
Juan Rodriguez <jurobega2@gmail.com>
2026-03-18 13:15:24 +0100

    migraciones, modelos

---
+++ savianDM/app/Models/Movil.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movil extends Model
+{
+    /** @use HasFactory<\Database\Factories\MovilFactory> */
+    use HasFactory;
+}
---
+++ savianDM/database/factories/CentroTrabajoFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\CentroTrabajo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<CentroTrabajo>
+ */
+class CentroTrabajoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
