a94ed8d0c1a4c48129c14f3d5afa8447e3ba491d
Asilper86 <asliper86@email.com>
2026-03-19 12:31:47 +0100

    Cambios 2

--- savianDM/resources/views/components/mios/base.blade.php
+++ savianDM/resources/views/components/mios/base.blade.php
@@ -1,9 +1,7 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="dark:bg-gray-800 overflow-hidden">
-                {{ $slot }}
-            </div>
+            {{ $slot }}
         </div>
     </div>
 </x-app-layout>
