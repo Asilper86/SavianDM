dcb6d0bdcff818402500ff85efb5e8edc18add3e
Juan Rodriguez <jurobega2@gmail.com>
2026-03-19 12:36:53 +0100

    liada

---
+++ savianDM/app/Http/components/mios/base.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="dark:bg-gray-800 overflow-hidden">
+                {{ $slot }}
+            </div>
+        </div>
+    </div>
+</x-app-layout>
