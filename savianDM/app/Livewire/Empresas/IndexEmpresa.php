fe924fc28ff7573c257f21329a0232bed8e35575
Asilper86 <asliper86@email.com>
2026-03-24 13:52:01 +0100

    final 240326

--- savianDM/app/Livewire/Empresas/IndexEmpresa.php
+++ savianDM/app/Livewire/Empresas/IndexEmpresa.php
@@ -6,14 +6,28 @@ use App\Models\CentroTrabajo;
 use App\Models\Empresa;
 use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class IndexEmpresa extends Component
 {
+    use WithPagination;
+    public string $orden = 'desc';
+    public string $campo = 'id';
+    public ?Empresa $empresa=null;
+
     #[On('evtEmpresaAnadida')]
     public function render()
     {
-        $centros_trabajo = CentroTrabajo::all();
-        $empresas = Empresa::select()->paginate(4);
-        return view('livewire.empresas.index-empresa', compact('centros_trabajo', 'empresas'));
+        $empresas = Empresa::orderBy($this->campo, $this->orden)->paginate(4);
+        return view('livewire.empresas.index-empresa', compact( 'empresas'));
+    }
+
+    public function ordenar(string $campo){
+        $this->orden=($this->orden=='asc') ? 'desc' : 'asc';
+        $this->campo=$campo;
+    }
+
+    public function borrar(){
+        $this->empresa->delete();  
     }
 }
--- savianDM/resources/views/livewire/empresas/index-empresa.blade.php
+++ savianDM/resources/views/livewire/empresas/index-empresa.blade.php
@@ -38,7 +38,9 @@
                             <td class="bg-slate-50/50 px-6 py-4 rounded-r-3xl border-y border-r border-transparent text-right">
                                 <div class="flex justify-end gap-2">
                                     <button class="p-2 bg-white text-slate-400 hover:text-[#07CBBB] rounded-lg transition-colors border border-slate-50"><svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" stroke-width="2"/></svg></button>
-                                    <button class="p-2 bg-white text-slate-400 hover:text-red-500 rounded-lg transition-colors border border-slate-50"><svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" stroke-width="2"/></svg></button>
+                                    <button wire:click="borrar" onclick="confirm('¿Desea eliminar definitivamente?')" class="p-2 bg-white text-slate-400 hover:text-red-500 rounded-lg transition-colors border border-slate-50">
+                                        <i class='fas fa-trash'></i>
+                                    </button>
                                 </div>
                             </td>
                         </tr>
