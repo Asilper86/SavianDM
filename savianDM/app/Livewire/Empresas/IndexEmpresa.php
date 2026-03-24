2530f71d2bb741f77f693771fc6ceca5a702a9af
Asilper86 <asliper86@email.com>
2026-03-24 13:11:50 +0100

    indexEmpresa Cambiado

--- savianDM/app/Livewire/Empresas/IndexEmpresa.php
+++ savianDM/app/Livewire/Empresas/IndexEmpresa.php
@@ -13,7 +13,7 @@ class IndexEmpresa extends Component
     public function render()
     {
         $centros_trabajo = CentroTrabajo::all();
-        $empresas = Empresa::select()->paginate(5);
+        $empresas = Empresa::select()->paginate(4);
         return view('livewire.empresas.index-empresa', compact('centros_trabajo', 'empresas'));
     }
 }
